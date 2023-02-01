4a2ff00498cd63f6c222a4b11a281ca75a495a0d
QiuPeng <paul0963913773@gmail.com>
2023-02-02 02:16:29 +0800

    finish CheckID

--- Homework/src/com/paul/wh4/CheckID.java
+++ Homework/src/com/paul/wh4/CheckID.java
@@ -21,14 +21,29 @@ public class CheckID {
 		return false;
 	}
 	
-	public static boolean check(String ID) {
-		int id1 = (ID.charAt(0) % 10);
-		
-		
-		int [] az = {10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33};
-		return true;
-	}
 	
+	
+	public static boolean check(String ID) {
+		int [] az = {10,11,12,13,14,15,16,17,34,18,19,20,21,22,35,23,24,25,26,27,28,29,32,30,31,33};
+		int id2 = az[ID.charAt(0) - 'A'];
+		int first = (id2 / 10) + (id2 % 10) * 9;
+		
+		int n = 8;
+		int p = 0;
+		int sum = 0;
+		for (int i = 1 ; i < 9 ; i++) {
+			int value = (ID.charAt(i) - 48) * n;
+			n--;
+			p += value;
+		}
+		 sum = 10 - (( p + first ) % 10);
+		System.out.println("驗證碼: " + sum);
+		if(sum == (ID.charAt(9) -48)) {
+			return true;
+		}
+		return false;
+	}	
+		
 	
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
@@ -37,7 +52,7 @@ public class CheckID {
 		String id = s.next();
 		id = id.toUpperCase();
 		
-		if (isID(id) == true) {
+		if (check(id) == true) {
 			System.out.println("身分證正確");
 		}
 		else {
